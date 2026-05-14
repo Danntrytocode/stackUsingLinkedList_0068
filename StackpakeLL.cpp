3adc00122316feb5816d58ef2c5313cfd8332a0c
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-14 22:00:58 +0700

    menambahkan operasi push  serta user input

--- StackpakeLL.cpp
+++ StackpakeLL.cpp
@@ -80,6 +80,13 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice){}
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        }
     }
 };
